781ca75800efe70e678b786b5b67c16a8a736d79
Tanner Howell <a02079061@usu.edu>
2023-04-05 19:06:56 -0600

    Updated variable names to make things clearer for myself

--- src/DisjointSet.java
+++ src/DisjointSet.java
@@ -1,13 +1,13 @@
 public class DisjointSet {
 
     // Array initial set up
-    private int [] ranks;
+    private int [] parentOrSize;
 
     // Constructor using a given size, ranks initially set to -1 for its size
     public DisjointSet(int size){
-        ranks = new int[size];
+        parentOrSize = new int[size];
         for (int i = 0; i < size; i++){
-            ranks[i] = - 1;
+            parentOrSize[i] = - 1;
         }
     }
 
@@ -17,23 +17,23 @@ public class DisjointSet {
         node2 = find(node2);
 
         // node2 is larger, so we add the size from node1 to node2 and make node 2 a new node
-        if (ranks[node1] < ranks[node2]){
-            ranks[node2] += ranks[node1];
-            ranks[node1] = node2;
+        if (parentOrSize[node1] < parentOrSize[node2]){
+            parentOrSize[node2] += parentOrSize[node1];
+            parentOrSize[node1] = node2;
         } else{
             // node1 is equal or larger
-            ranks[node1] += ranks[node2];
-            ranks[node2] = node1;
+            parentOrSize[node1] += parentOrSize[node2];
+            parentOrSize[node2] = node1;
         }
     }
 
     public int find(int node){
         // A negative value here would mean the node is a root node
-        if (ranks[node] < 0){
+        if (parentOrSize[node] < 0){
             return node;
         } else{ // if it's not a root, then find the root, update the parent of the node to point to the root and return it
-            ranks[node] = find(ranks[node]);
-            return ranks[node];
+            parentOrSize[node] = find(parentOrSize[node]);
+            return parentOrSize[node];
         }
     }
 
