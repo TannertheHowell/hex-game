0a3382e5c487a3bccdcc1794c4cf489c61a25b7c
Tanner Howell <a02079061@usu.edu>
2023-04-06 17:49:03 -0600

    Added a constructor for HexGame, updated isOccupied asd well

--- src/HexGame.java
+++ src/HexGame.java
@@ -2,6 +2,29 @@ import java.util.ArrayList;
 
 public class HexGame {
 
+    private Color[] grid;
+    private int size;
+    private DisjointSet redPlayer;
+    private DisjointSet bluePlayer;
+
+    // Extra edge elements
+    private final int TOP_EDGE = size + 1;
+    private final int BOTTOM_EDGE = size + 2;
+    private final int LEFT_EDGE = size + 3;
+    private final int RIGHT_EDGE = size + 4;
+
+    // Constructor
+    public HexGame(int size){
+        this.size = size;
+        grid = new Color[size * size + 1];
+        for(int i = 1; i <= size * size; i++){
+            grid[i] = Color.None;
+        }
+        redPlayer = new DisjointSet(size * size + 4);
+        bluePlayer = new DisjointSet(size * size + 4);
+    }
+
+
     // Position is an int from 1 to 121
     // When displayNeighbors is true, the hex positions of neighboring cells are shown
     // Return true if blue has won, otherwise false for not winning or if the space is full already
@@ -25,11 +48,11 @@ public class HexGame {
     }
 
     public int getSize(){
-        return this.size;
+        return size;
     }
 
     private boolean isOccupied(int position){
-        return false;
+        return grid[position] != Color.None;
     }
 
     private ArrayList<Integer> getNeighborsRed(int position){
